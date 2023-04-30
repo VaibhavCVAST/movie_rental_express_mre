334930f962a9436279d3c0981dad3ed6156f6cd3
VaibhavGC <vaibhavc@valueaddsofttech.com>
2023-05-01 01:01:20 +0530

    uncommented some lines no big change

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('express-async-error')
 const express = require('express')
 const app = express()
 const startServer = require('./startup/port')
-// const Process = require('./startup/logging');
+const Process = require('./startup/logging');
 const allRoutes = require("./startup/routes");
 app.use(express.json());
-// app.use(Process)
-allRoutes(app)
+app.use(Process)
 startServer(app)
+allRoutes(app)
 
 module.exports = app
